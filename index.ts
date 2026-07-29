78910407e04ea86aebb1c37f36b965c48a5d5090
Yuvadi29 <adityatrivedi612@gmail.com>
2026-07-29 10:21:21 +0530

    Initializing the Commander Module

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+    .name("adiclaw")
+    .description("AdiClaw CLI Tool")
+    .version("0.0.1");
+
+program
+    .command("new")
+    .description("Create a new adiclaw project")
+    .action(async () => {
+        console.log("Creating new adiclaw project...");
+    });
+
+program
+    .command("wakeup")
+    .description("Show the Banner and pick cli or telegram mode")
+    .action(
+        async () => {
+            console.log("Wakeup Calling...");
+        }
+    );
+
+await program.parseAsync(process.argv);
