7e01200cec21765dc4f4b2ce9f477bf36786438b
Yuvadi29 <adityatrivedi612@gmail.com>
2026-07-29 22:48:15 +0530

    Creating the agent tools for CRUD file/directory

--- modes/agent/orchestrator.ts
+++ modes/agent/orchestrator.ts
@@ -3,6 +3,7 @@ import chalk from "chalk";
 import { defaultAgentConfig } from "./types";
 import { ActionTracker } from "./action-tracker";
 import { ToolExecutor } from "./tool-executor";
+import { createAgentTools } from "./agent-tools";
 
 export async function runAgentMode() {
     console.log(chalk.bold("Running Agent Mode"));
@@ -18,5 +19,6 @@ export async function runAgentMode() {
     const tracker = new ActionTracker();
     //    This will be our tool executor which takes care of executing the tools
     const executor = new ToolExecutor(tracker, config);
-
+    // This is where we will be creating the tools that the agent can use
+    const tools = createAgentTools(executor);
 }
